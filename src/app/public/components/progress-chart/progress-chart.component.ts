180a8d2c8a752cfd8f5ce7fb393901b4a5a6a0d2
NeoRise456 <defneorise@gmail.com>
2025-10-21 18:42:23 -0500

    feat: integrate ng-apexcharts for progress chart visualization

--- src/app/public/components/progress-chart/progress-chart.component.ts
+++ src/app/public/components/progress-chart/progress-chart.component.ts
@@ -1,21 +1,93 @@
-import { Component } from '@angular/core';
+import { Component ,ViewChild} from '@angular/core';
+
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexTooltip,
+  ApexStroke
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  stroke: ApexStroke;
+  tooltip: ApexTooltip;
+  dataLabels: ApexDataLabels;
+};
+
 
 @Component({
   selector: 'app-progress-chart',
-  imports: [],
+  imports: [
+    ChartComponent
+  ],
   template: `
     <div class="bg-card block border p-6 rounded-lg shadow-sm text-card-foreground w-full">
       <div class="flex flex-col">
         <span class="text-xl font-semibold text-foreground ">Score Progress</span>
         <span class="text-xs text-muted-foreground font-semibold tracking-widest">Last 7 days</span>
       </div>
-      <div>
-        chart
+      <div class="">
+        <apx-chart
+          [series]="chartOptions.series"
+          [chart]="chartOptions.chart"
+          [xaxis]="chartOptions.xaxis"
+          [stroke]="chartOptions.stroke"
+          [tooltip]="chartOptions.tooltip"
+          [dataLabels]="chartOptions.dataLabels"
+        ></apx-chart>
       </div>
     </div>
   `,
   styles: ``
 })
 export class ProgressChartComponent {
+  @ViewChild("chart") chart: ChartComponent | undefined;
+  public chartOptions: ChartOptions;
 
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "series1",
+          data: [31, 40, 28, 51, 42, 109, 100]
+        },
+        {
+          name: "series2",
+          data: [11, 32, 45, 32, 34, 52, 41]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "area"
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: "smooth"
+      },
+      xaxis: {
+        type: "datetime",
+        categories: [
+          "2018-09-19T00:00:00.000Z",
+          "2018-09-19T01:30:00.000Z",
+          "2018-09-19T02:30:00.000Z",
+          "2018-09-19T03:30:00.000Z",
+          "2018-09-19T04:30:00.000Z",
+          "2018-09-19T05:30:00.000Z",
+          "2018-09-19T06:30:00.000Z"
+        ]
+      },
+      tooltip: {
+        x: {
+          format: "dd/MM/yy HH:mm"
+        }
+      }
+    };
+  }
 }
