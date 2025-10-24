8c999dfd3c25229270ae46d30cb0628ece1839af
Al3xiel <gabriel280104casas@gmail.com>
2025-10-24 11:16:18 -0500

    feat(settings): add switch component.

--- src/app/settings/pages/settings-page.component/settings-page.component.ts
+++ src/app/settings/pages/settings-page.component/settings-page.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {ZardSwitchComponent} from '@shared/components/switch/switch.component';
 
 @Component({
   selector: 'app-settings-page',
-  imports: [],
+  imports: [
+    ZardSwitchComponent
+  ],
   templateUrl: './settings-page.component.html',
   styleUrl: './settings-page.component.css'
 })
