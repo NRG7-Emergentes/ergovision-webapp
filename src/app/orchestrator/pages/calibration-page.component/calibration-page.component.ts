c678b00c483c8d40ddc311b635a23474cbe69905
Al3xiel <gabriel280104casas@gmail.com>
2025-10-26 11:52:07 -0500

    feat(settings): add calibration details.

--- src/app/orchestrator/pages/calibration-page.component/calibration-page.component.ts
+++ src/app/orchestrator/pages/calibration-page.component/calibration-page.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import {CameraViewComponent} from '@app/orchestrator/components/calibration-camera/camera-view.component';
+import {ZardIconComponent} from '@shared/components/icon/icon.component';
+import {ZardButtonComponent} from '@shared/components/button/button.component';
 
 
 @Component({
   selector: 'app-calibration-page',
   imports: [
-    CameraViewComponent
+    CameraViewComponent,
+    ZardIconComponent,
+    ZardButtonComponent
   ],
   templateUrl: './calibration-page.component.html',
   styleUrl: './calibration-page.component.css'
