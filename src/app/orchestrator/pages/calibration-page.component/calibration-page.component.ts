70c7c1ed1cfb3a9ed00badda7b0c3690ab5aa0bb
Al3xiel <gabriel280104casas@gmail.com>
2025-10-26 09:46:58 -0500

    feat(settings): add camera component.

--- src/app/orchestrator/pages/calibration-page.component/calibration-page.component.ts
+++ src/app/orchestrator/pages/calibration-page.component/calibration-page.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {CameraViewComponent} from '@app/orchestrator/components/calibration-camera/camera-view.component';
+
 
 @Component({
   selector: 'app-calibration-page',
-  imports: [],
+  imports: [
+    CameraViewComponent
+  ],
   templateUrl: './calibration-page.component.html',
   styleUrl: './calibration-page.component.css'
 })
