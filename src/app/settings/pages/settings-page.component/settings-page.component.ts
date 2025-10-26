e004951e4fee9014c8a24e0eedb899addb84cfba
Al3xiel <gabriel280104casas@gmail.com>
2025-10-25 21:47:31 -0500

    feat(settings): enhance settings options.

--- src/app/settings/pages/settings-page.component/settings-page.component.ts
+++ src/app/settings/pages/settings-page.component/settings-page.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import {ZardSwitchComponent} from '@shared/components/switch/switch.component';
+import {ZardSliderComponent} from '@shared/components/slider/slider.component';
+import {ZardSelectComponent} from '@shared/components/select/select.component';
+import {ZardSelectItemComponent} from '@shared/components/select/select-item.component';
+import {ZardButtonComponent} from '@shared/components/button/button.component';
 
 @Component({
   selector: 'app-settings-page',
   imports: [
-    ZardSwitchComponent
+    ZardSwitchComponent,
+    ZardSliderComponent,
+    ZardSelectComponent,
+    ZardSelectItemComponent,
+    ZardButtonComponent
   ],
   templateUrl: './settings-page.component.html',
   styleUrl: './settings-page.component.css'
