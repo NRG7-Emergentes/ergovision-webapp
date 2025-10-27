7b966132821485cf1b06e5fb2f416872f7f08269
NeoRise456 <defneorise@gmail.com>
2025-10-27 01:34:44 -0500

    fix: update pose landmarker model to heavy version for improved accuracy

--- src/app/monitoring/components/monitor-cam/monitor-cam.component.ts
+++ src/app/monitoring/components/monitor-cam/monitor-cam.component.ts
@@ -141,7 +141,7 @@ export class MonitorCamComponent {
     const poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
       baseOptions: {
         modelAssetPath:
-          'https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task',
+          'https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_heavy/float16/1/pose_landmarker_heavy.task',
         delegate: 'GPU'
       },
       runningMode: 'VIDEO'
