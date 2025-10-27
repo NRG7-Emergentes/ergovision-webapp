1f752ef40ee3532236cff0c2d9b548444d06de79
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-10-26 23:28:11 -0500

    feat: add sign-in , sign-up and profile path.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,17 @@ import { Routes } from '@angular/router';
 import {MainDashboardComponent} from '@app/public/pages/main-dashboard/main-dashboard.component';
 import {TestPageComponent} from '@app/public/pages/test-page/test-page.component';
 import {MonseCamComponent} from '@app/public/pages/monse-cam/monse-cam.component';
+import {ProfileComponent} from '@app/profiles/presentation/components/profile/profile.component';
+import {SignInComponent} from '@app/iam/presentation/components/sign-in/sign-in.component';
+import {SignUpComponent} from '@app/iam/presentation/components/sign-up/sign-up.component';
 
 export const routes: Routes = [
+
+
   { path: 'dashboard/:id' , component: MainDashboardComponent},
   {path: 'monse', component: MonseCamComponent},
+  { path: 'sign-in', component: SignInComponent},
+  { path: 'sign-up', component: SignUpComponent},
+  {path: 'profile', component: ProfileComponent},
   { path: '' , component: TestPageComponent }
 ];
