70311a0d031be8c27ff2885eea2cfa87db1e37b2
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-11-12 00:21:53 -0500

    feat: add getUserMe and updateProfile methods to UserService

--- src/app/iam/services/user.service.ts
+++ src/app/iam/services/user.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 import { BaseService } from "@shared/services/base.service";
 import { HttpClient } from '@angular/common/http';
 import { User } from '../domain/model/user.entity';
+import { Observable} from 'rxjs';
+import {UpdateUserRequest} from '@app/iam/domain/model/update-user.request';
+import {SignUpResponse} from '@app/iam/domain/model/sign-up.response';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,4 +14,13 @@ export class UserService extends BaseService<User>{
     super(http);
     this.resourceEndpoint = '/users';
   }
+
+  getUserMe(): Observable<User> {
+    return this.http.get<User>(`${this.basePath}${this.resourceEndpoint}/me`);
+  }
+
+  updateProfile(userData: UpdateUserRequest): Observable<SignUpResponse> {
+    return this.http.put<SignUpResponse>(`${this.basePath}${this.resourceEndpoint}`, userData);
+  }
+
 }
