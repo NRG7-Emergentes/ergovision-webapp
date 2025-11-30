a91584af1cdb525cf86cb4663871efbf776bc124
NeoRise456 <defneorise@gmail.com>
2025-11-30 00:51:03 -0500

    feat(authentication): enhance auto sign-in to restore user data from localStorage

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,31 +1,45 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { inject, OnInit } from '@angular/core';
+import { inject } from '@angular/core';
 import { DarkModeService } from '@shared/services/darkMode.service';
 import {NavbarComponent} from '@app/public/components/navbar/navbar.component';
 import {FooterComponent} from '@app/public/components/footer/footer.component';
+import {ZardToastComponent} from '@shared/components/toast/toast.component';
+import {AuthenticationService} from '@app/iam/services/authentication.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, ZardToastComponent],
   template: `
-    <div class="grid min-h-dvh grid-rows-[auto_1fr_auto] bg-background font-inter">
-      <app-navbar/>
-      <div class="">
-        <router-outlet  />
-      </div>
-      <app-footer/>
+    <div class="font-inter ">
+      @if(authService.isSignedIn()) {
+        <div class="grid min-h-dvh grid-rows-[auto_1fr_auto]">
+          <app-navbar/>
+          <div class="">
+            <router-outlet  />
+            <z-toaster />
+          </div>
+          <app-footer/>
+        </div>
+      } @else {
+        <div class="min-h-dvh">
+          <router-outlet  />
+          <z-toaster />
+        </div>
+      }
     </div>
   `,
   styles: `
   `
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('ergovision-frontend');
 
   private readonly darkModeService = inject(DarkModeService);
+  protected readonly authService = inject(AuthenticationService);
 
   ngOnInit(): void {
     this.darkModeService.initTheme();
+    this.authService.tryAutoSignIn();
   }
 }
